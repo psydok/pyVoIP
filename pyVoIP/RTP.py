57e30c31d60aeb3d32ad16420b18b34de876ccdc
psydok <47638600+psydok@users.noreply.github.com>
2025-11-17 11:37:27 +0500

    Update RTP.py

--- pyVoIP/RTP.py
+++ pyVoIP/RTP.py
@@ -474,8 +474,7 @@ class RTPClient:
         return self.parse_pcmu(packet)
 
     def parse_pcmu(self, packet: RTPMessage) -> None:
-        data = audioop.ulaw2lin(packet.payload, 1)
-        data = audioop.bias(data, 1, 200)
+        data = audioop.ulaw2lin(packet.payload, 2)
         self.pmin.write(packet.timestamp, data)
 
     def encodePCMU(self, packet: bytes) -> bytes:
@@ -488,8 +487,7 @@ class RTPClient:
         return self.encode_pcmu(packet)
 
     def encode_pcmu(self, packet: bytes) -> bytes:
-        packet = audioop.bias(packet, 1, -200)
-        packet = audioop.lin2ulaw(packet, 1)
+        packet = audioop.lin2ulaw(packet, 2)
         return packet
 
     def parsePCMA(self, packet: RTPMessage) -> None:
@@ -502,8 +500,7 @@ class RTPClient:
         return self.parse_pcma(packet)
 
     def parse_pcma(self, packet: RTPMessage) -> None:
-        data = audioop.alaw2lin(packet.payload, 1)
-        data = audioop.bias(data, 1, 128)
+        data = audioop.alaw2lin(packet.payload, 2)
         self.pmin.write(packet.timestamp, data)
 
     def encodePCMA(self, packet: bytes) -> bytes:
@@ -516,8 +513,7 @@ class RTPClient:
         return self.encode_pcma(packet)
 
     def encode_pcma(self, packet: bytes) -> bytes:
-        packet = audioop.bias(packet, 1, -128)
-        packet = audioop.lin2alaw(packet, 1)
+        packet = audioop.lin2alaw(packet, 2)
         return packet
 
     def parseTelephoneEvent(self, packet: RTPMessage) -> None:
